57dad7f856fb0bbdd6b57f2422c0fd9cee11d960
ignacioborraz <ignacioborraz@hotmail.com>
2022-05-16 19:55:19 -0300

    new controllers

--- controllers/petControllers.js
+++ controllers/petControllers.js
@@ -1,27 +1,35 @@
 const db = require("../models")
+const Pet = db.Pet
 
 const petControllers = {
 
     index: (req,res)=>{return res.render('index')},
 
+    createPets: async (req,res)=>{
+        let {name,tag} = req.body
+        try {
+            let pet = await Pet.create({name,tag})
+            let responses = {
+                code: 201,
+                description: "null response"
+            }
+            return res.status(201).json(responses)
+        } catch(error) {
+            console.log(error)
+            return res.status(400).json({code: 400, message: "unexpected error"})
+        }
+    },
+
     listPets: async (req,res)=>{
         try {
-            let pets = await db.Pet.findAll()
+            let pets = await Pet.findAll()
             if (pets) {
-                let list = {
-                    summary: "List all pets",
-                    operationId: "listPets",
-                    tags: ["pets"],
-                    parameters: {
-                        name: "limit",
-                        in: "query",
-                        required: false,
-                        description: "How many items to return at one time (max 100)",
-                        schema: {type: "string"}
-                    },
-                    responses: {code: 200, description: "A paged array of pets", content: JSON.stringify(pets)}
+                let responses = {
+                    code: 200,
+                    description: "A paged array of pets",
+                    content: pets
                 }
-                return res.status(200).render('listPets',{list})
+                return res.status(200).json(responses)
             } else {
                 return res.status(404).json({code: 404, message: "null response"})
             }
@@ -30,75 +38,61 @@ const petControllers = {
         }
     },
 
-    createPets: async (req,res)=>{
-        let {name,tag} = req.body
-        console.log(req.body)
+    showPetById: async (req,res)=>{
+        let id = req.params.id
         try {
-            let pet = await db.Pet.create({name,tag})
-            let create = {
-                summary: "Create a pet",
-                operationId: "createPets",
-                responses: {code: 201, description: "null response"}
+            let pet = await Pet.findOne({where: {id:id}})
+            if (pet) {
+                let responses = {
+                    code: 200,
+                    description: "Expected response to a valid request",
+                    content: pet
+                }
+                return res.status(200).json(responses)
+            } else {
+                return res.status(404).json({code: 404, description: "not found"})
             }
-            return res.status(201).json(create.responses)
         } catch(error) {
-            console.log(error)
             return res.status(400).json({code: 400, message: "unexpected error"})
         }
     },
 
-    showPetById: async (req,res)=>{
+    updatePetById: async (req,res)=>{
         let id = req.params.id
         try {
-            let pet = await db.Pet.findByPk(id)
+            let pet = await Pet.findOne({where: {id:id}})
             if (pet) {
-                let byId = {
-                    summary: "Info for a specific pet",
-                    operationId: "showPetById",
-                    tags: ["pets"],
-                    parameters: {
-                        name: "petId",
-                        in: "path",
-                        required: true,
-                        description: "The id of the pet to retrieve",
-                        schema: {type: "string"}
-                    },
-                    responses: {code: 200, description: "Expected response to a valid request", content: JSON.stringify(pet)}
+                await Pet.update(req.body, {where: {id:id}})
+                let responses = {
+                    code: 201,
+                    description: "null response"
                 }
-                return res.status(200).render('showPetById',{byId})
+                return res.status(201).json(responses)
             } else {
                 return res.status(404).json({code: 404, description: "not found"})
             }
         } catch(error) {
             return res.status(400).json({code: 400, message: "unexpected error"})
         }
-/*     },
+    },
 
-    modifyPetById: async (req,res)=>{
+    deletePetById: async (req,res)=>{
         let id = req.params.id
         try {
-            let pet = await db.Pet.findByPk(id)
+            let pet = await Pet.findOne({where: {id:id}})
             if (pet) {
-                let byId = {
-                    summary: "Info for a specific pet",
-                    operationId: "showPetById",
-                    tags: ["pets"],
-                    parameters: {
-                        name: "petId",
-                        in: "path",
-                        required: true,
-                        description: "The id of the pet to retrieve",
-                        schema: {type: "string"}
-                    },
-                    responses: {code: 200, description: "Expected response to a valid request", content: JSON.stringify(pet)}
+                await Pet.destroy({where: {id:id}})
+                let responses = {
+                    code: 201,
+                    description: "null response"
                 }
-                return res.status(200).render('showPetById',{byId})
+                return res.status(201).json(responses)
             } else {
                 return res.status(404).json({code: 404, description: "not found"})
             }
         } catch(error) {
             return res.status(400).json({code: 400, message: "unexpected error"})
-        } */
+        }
     }
 
 }
--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,11 @@
 const router = require('express').Router()
-const {index,listPets,showPetById,createPets} = require('../controllers/petControllers')
+const {index,listPets,showPetById,updatePetById,deletePetById,createPets} = require('../controllers/petControllers')
 
+router.post('/pets', createPets)
 router.get('/', index)
 router.get('/pets', listPets)
 router.get('/pets/:id', showPetById)
-router.post('/pets', createPets)
+router.put('/pets/:id', updatePetById)
+router.delete('/pets/:id', deletePetById)
 
 module.exports = router
