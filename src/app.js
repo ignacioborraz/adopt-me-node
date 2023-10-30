4f986342964457d292744454c99dd079bb07a988
ignacio.borraz <ignacioborraz@hotmail.com>
2023-10-30 14:43:01 -0300

    fix

--- src/app.js
+++ src/app.js
@@ -1,12 +1,8 @@
 import "dotenv/config.js"
-import config from "./config/swagger.js"
 
 import express from "express";
-import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
 import morgan from "morgan";
-import swaggerJSDoc from "swagger-jsdoc"
-import { serve,setup } from "swagger-ui-express"
 
 import router from "./routes/index.js"
 
@@ -16,13 +12,10 @@ import notFoundHandler from "./middlewares/notFoundHandler.js"
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-const specs = swaggerJSDoc(config)
-
 app.use(express.urlencoded({ extended: true}))
 app.use(express.json());
 app.use(cookieParser());
 app.use(morgan('dev'))
-app.use("/api/docs",serve,setup(specs))
 
 app.use("/api",router)
 app.use(errorHandler)
