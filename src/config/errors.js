749c908aa3b43640aa62f33b63f72f513cef46a3
ignacio.borraz <ignacioborraz@hotmail.com>
2023-10-30 21:23:01 -0300

    errors

--- src/config/errors.js
+++ src/config/errors.js
@@ -1,3 +1,7 @@
 export default {
-
+  notFoundOne: { statusCode: 404, status: "error", message: "Not found document" },
+  notFound: { statusCode: 404, status: "error", message: "Not found documents" },
+  incomplete: { statusCode: 400, status: "error", message: "Incomplete values" },
+  auth: { statusCode: 401, status: "auth", message: "Invalid credentials" },
+  forbidden: { statusCode: 403, status: "forbidden", message: "Not Allowed" }
 }
